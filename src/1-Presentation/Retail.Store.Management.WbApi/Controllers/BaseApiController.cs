0e708ff980bd5913b4194a24ef9c5548f3d99514
bmassioui <bouchaib.massioui@gmail.com>
2023-04-29 13:11:09 +0100

    :package: Define Domain.Common items

---
+++ src/1-Presentation/Retail.Store.Management.WbApi/Controllers/BaseApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Retail.Store.Management.WbApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public abstract class BaseApiController : ControllerBase
+{
+}
---
+++ src/2-Core/Retail.Store.Management.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Retail.Store.Management.Domain.Common;
+
+public abstract class BaseAuditableEntity<TId> : BaseEntity<TId> where TId : struct, IEquatable<TId>
+{
+    public DateTime CreatedAt { get; set; }
+
+    public TId? CreatedBy { get; set; }
+
+    public DateTime? LastModifiedAt { get; set; }
+
+    public TId? LastModifiedBy { get; set; }
+}
